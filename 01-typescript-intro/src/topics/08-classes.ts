9f57a035f2c9feca7b57981f417a5b7fa8837b30
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-06-12 23:52:51 -0300

    constructor de una clase en ts

--- 01-typescript-intro/src/topics/08-classes.ts
+++ 01-typescript-intro/src/topics/08-classes.ts
@@ -1,15 +1,15 @@
 
 
 export class Person {
-    public name?: string;
-    private address: string;
+    // public name?: string;
+    // private address: string;
 
-    constructor() {
-        this.name = 'Fiorela';
-        this.address = 'Italia';
-    }
+    constructor( 
+        public name: string, 
+        private address: string = 'No Address'
+    ) {}
 }
 
-const ironman =  new Person();
+const ironman =  new Person('Iron Man', 'Ney York');
 
 console.log(ironman);
