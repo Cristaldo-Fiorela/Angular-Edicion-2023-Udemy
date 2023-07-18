9579a4c49838d286d779930526b3ce1f29778938
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2023-07-17 23:16:00 -0300

    creacion de componente se barra de busqueda

--- 03-gifs-app/src/app/gifs/gifs.module.ts
+++ 03-gifs-app/src/app/gifs/gifs.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home/home-page.component';
+import { SearchBoxComponent } from './components/search-box/search.component';
 
 
 @NgModule({
   declarations: [
-    HomePageComponent
+    HomePageComponent,
+    SearchBoxComponent
   ],
   imports: [
     CommonModule
